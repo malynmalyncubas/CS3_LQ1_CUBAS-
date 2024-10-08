5e12666371f201bfcb5e73251bf0ef3460ffec55
malynmalyncubas <cubasaumalyn@gmail.com>
2024-10-08 19:37:48 +0800

    Add files via upload

---
+++ quizone.py
@@ -0,0 +1,25 @@
+#variable that stores of groupmates
+MemberName1="Ruth Ann Gaygay-ed"
+MemberName2="Yuri Gagarin"
+Age_Member3="Aumalyn Cubas"
+
+#create variables that contains their age in whole number
+Age_Member1=str(20)
+Age_Member2=str(20)
+Age_Member3=str(20)
+
+#create variables that contains their weekly allowance in decimal form
+All_Member1=float(1000)
+All_Member2=float(2000)
+All_Member3=float(2000)
+
+#print all results formatted as follows: use contact
+("Team Name:")
+Member1:",(Name_Member1),",her age is,(Age_Member),",allowance per week is",(All_Member1))
+Member2:",(Name_Member2),",her age is,(Age_Member),",allowance per week is",(All_Member2))
+Member3:",(Name_Member3),",her age is,(Age_Member),",allowance per week is",(All_Member3))
+
+#variable to store the lenght of the names of the members and print them as follows:
+Member 1 consists of", len(Name_Member1), "characters")
+Member 2 consists of", len(Name_Member2), "characters")
+Member 1 consists of", len(Name_Member1), "characters")
